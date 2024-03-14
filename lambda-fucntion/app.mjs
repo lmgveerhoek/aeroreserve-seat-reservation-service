307ac997175c33d7e054029c2326479c7a18b426
Niels Arts <niels.arts@student.uva.nl>
2024-03-14 13:45:42 +0100

    added secret manager to get zeebe-credentials.

--- lambda-fucntion/app.mjs
+++ lambda-fucntion/app.mjs
@@ -11,11 +11,81 @@
  * 
  */
 
+import { v4 as uuidv4 } from 'uuid';
+
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "zeebe-credentials";
+
+const client = new SecretsManagerClient({
+  region: "eu-north-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = response.SecretString;
+
+// Your code goes here
+////////////////////////////////////
+// FAKE SEAT RESERVATION SERVICE
+////////////////////////////////////
+import { ZBClient } from "zeebe-node";
+
+const zeebeClient = new ZBClient({
+	camundaCloud: {
+		clientId: secret.ZEEBE_CLIENT_ID,
+		clientSecret: secret.ZEEBE_CLIENT_SECRET,
+		clusterId: secret.ZEEBE_CLUSTER_ID,
+	},
+})
+
+const worker = zeebeClient.createWorker('reserve-seats', reserveSeatsHandler)
+
+function reserveSeatsHandler(job, _, worker) {  
+  console.log("\n\n Reserve seats now...");
+  console.log(job);
+
+  // Do the real reservation
+  // TODO: Fake some results! Fake an error (when exactly?)
+  if ("seats" !== job.variables.simulateBookingFailure) {
+    console.log("Successul :-)");
+    return job.complete({
+        reservationId: "1234",
+      });
+  } else {
+    console.log("ERROR: Seats could not be reserved!");
+    return job.error("ErrorSeatsNotAvailable");
+  }
+}
+
 export const lambdaHandler = async (event, context) => {
+
     const response = {
       statusCode: 200,
+      headers: {
+          "Content-Type": "application/json"
+      },
       body: JSON.stringify({
-        message: 'hello world',
+          message: "Serverless fucntion executed successfully!"
       })
     };
 
